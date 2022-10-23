57f6e59042f5497f7fcf2c0aec45b1fdd9ac29a4
Federico Matteoni <fedex15@live.it>
2022-10-23 17:42:29 +0200

    Android - Spell element dialog

--- Caput_Draconis/src/main/java/com/fexed/caputdraconis/android/SpellListActivity.kt
+++ Caput_Draconis/src/main/java/com/fexed/caputdraconis/android/SpellListActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -20,6 +22,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.fexed.caputdraconis.AndroidSpellsLoader.Companion.CSVFile
 import com.fexed.caputdraconis.Spell
 
@@ -88,35 +91,81 @@ class SpellListActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SpellListElement(spell: Spell) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentHeight()
-        .padding(8.dp)) {
-        Text(text = spell.nome,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            modifier = Modifier.fillMaxWidth())
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Text(text = spell.categoria,
-                fontSize = 14.sp)
-            Text(text = spell.fonte,
-                fontSize = 10.sp,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.End)
+    var showSpellDialog by remember { mutableStateOf(false)}
+
+    if (showSpellDialog) SpellDialog(spell = spell) { showSpellDialog = false }
+
+    Surface(onClick = {showSpellDialog = true}) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp)) {
+            Text(text = spell.nome,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                modifier = Modifier.fillMaxWidth())
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = spell.categoria,
+                    fontSize = 14.sp)
+                Text(text = spell.fonte,
+                    fontSize = 10.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End)
+            }
+        }
+    }
+}
+
+@Composable
+fun SpellDialog(spell: Spell, onClose: () -> Unit) {
+    Dialog(onDismissRequest = onClose) {
+        Surface(shape = MaterialTheme.shapes.large, elevation = 10.dp, modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()) {
+            Box(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                Column(modifier = Modifier
+                    .padding(16.dp)
+                    .wrapContentSize()) {
+                    Text(text = spell.nome,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        modifier = Modifier.fillMaxWidth())
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = spell.categoria, fontSize = 12.sp)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Difesa: ${spell.difinc}", fontSize = 12.sp)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = spell.descrizione, modifier = Modifier.fillMaxWidth())
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = spell.fonte,
+                        fontSize = 10.sp,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.End)
+                }
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun DefaultPreview() {
+fun ListPreview() {
     CaputDraconisTheme {
         Column {
             SpellListElement(Spell("Nome", "Descrizione", "Difesa", "Categoria", "Fonte"))
             SpellListElement(Spell("Nome", "Descrizione", "Difesa", "Categoria", "Fonte"))
             SpellListElement(Spell("Nome", "Descrizione", "Difesa", "Categoria", "Fonte"))
         }
+    }
+}
+
+@Preview
+@Composable
+fun DialogPreview() {
+    CaputDraconisTheme {
+        SpellDialog(Spell("Nome", "Descrizione molto molto lunga\nDescrizione molto molto lunga\nDescrizione molto molto lunga\nDescrizione molto molto lunga\n", "Difesa", "Categoria", "Fonte")) {}
     }
 }
