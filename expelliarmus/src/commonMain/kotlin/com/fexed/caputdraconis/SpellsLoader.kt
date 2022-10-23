75a8085ad52a0cc6b880ed4d96736d8aa84cc0db
Federico Matteoni <fedex15@live.it>
2022-10-23 17:26:42 +0200

    Common - Removed logs

--- expelliarmus/src/commonMain/kotlin/com/fexed/caputdraconis/SpellsLoader.kt
+++ expelliarmus/src/commonMain/kotlin/com/fexed/caputdraconis/SpellsLoader.kt
@@ -3,15 +3,12 @@ package com.fexed.caputdraconis
 interface SpellsLoader {
     fun GetCSVFromAssets(): String
     fun LoadSpells(): List<Spell> {
-        println("Common:SPELLS\tLoading spells")
         val list = ArrayList<Spell>()
         val CSV = GetCSVFromAssets()
-        println("Common:SPELLS\tCSV Loaded: " + CSV.length + " chars")
 
         val lines = CSV.split("\n")
 
         for (line in lines) {
-            println("Common:SPELLS\t" + line)
             if (line != "") {
                 val rawSpell = line.split("\t")
                 val parsedSpell = Spell(
@@ -24,7 +21,6 @@ interface SpellsLoader {
                 list.add(parsedSpell)
             }
         }
-        println("Common:SPELLS\tSpell list created: " + list.size + " spells")
         return list
     }
 }
