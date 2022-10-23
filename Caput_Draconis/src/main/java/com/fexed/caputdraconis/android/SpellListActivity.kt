d36756dddc76a40c10ac7a161bb06925f6295b1b
Federico Matteoni <fedex15@live.it>
2022-10-23 18:51:01 +0200

    Android - More iOS-like UI

--- Caput_Draconis/src/main/java/com/fexed/caputdraconis/android/SpellListActivity.kt
+++ Caput_Draconis/src/main/java/com/fexed/caputdraconis/android/SpellListActivity.kt
@@ -1,22 +1,16 @@
 package com.fexed.caputdraconis.android
 
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,45 +21,6 @@ import com.fexed.caputdraconis.AndroidSpellsLoader.Companion.CSVFile
 import com.fexed.caputdraconis.Spell
 
 import com.fexed.caputdraconis.getSpellsLoader
-
-@Composable
-fun CaputDraconisTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        darkColors(
-            primary = Color(0xFFBB86FC),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
-    } else {
-        lightColors(
-            primary = Color(0xFF6200EE),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
-    }
-    val typography = Typography(
-        body1 = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
-        )
-    )
-    val shapes = Shapes(
-        small = RoundedCornerShape(4.dp),
-        medium = RoundedCornerShape(4.dp),
-        large = RoundedCornerShape(0.dp)
-    )
-
-    MaterialTheme(
-        colors = colors,
-        typography = typography,
-        shapes = shapes,
-        content = content
-    )
-}
 
 class SpellListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -80,13 +35,21 @@ class SpellListActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LazyColumn {
-                        items(items = spells, itemContent =  { spell ->
-                            SpellListElement(spell = spell)
-                        })
-                    }
+                    SpellList(spells = spells)
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun SpellList(spells: List<Spell>) {
+    Surface(shape = MaterialTheme.shapes.large, modifier = Modifier.padding(horizontal = 16.dp), elevation = 10.dp) {
+        LazyColumn {
+            items(items = spells, itemContent =  { spell ->
+                SpellListElement(spell = spell)
+                Divider()
+            })
         }
     }
 }
