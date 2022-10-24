df281b5c89d25481cd33c422809e22e42b900b14
Federico Matteoni <fedex15@live.it>
2022-10-24 21:25:07 +0200

    iOS - update Platform

--- expelliarmus/src/iosMain/kotlin/com/fexed/caputdraconis/Platform.kt
+++ expelliarmus/src/iosMain/kotlin/com/fexed/caputdraconis/Platform.kt
@@ -1,10 +1,8 @@
 package com.fexed.caputdraconis
 
-import platform.UIKit.UIDevice
-
 class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    override val version: String = "Expelliarmus v0.1\niOS v0.1"
+    override val name: String = "iOS"
+    override val appVersion: String = "v0.1"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
