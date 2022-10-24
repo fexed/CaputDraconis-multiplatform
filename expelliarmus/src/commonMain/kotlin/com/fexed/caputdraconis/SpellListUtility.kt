1316c25f2b9270dc2ee3958d9fd43c0106ad25d7
Federico Matteoni <fedex15@live.it>
2022-10-24 21:25:32 +0200

    Common - Spell list utilities (search)

---
+++ expelliarmus/src/commonMain/kotlin/com/fexed/caputdraconis/SpellListUtility.kt
@@ -0,0 +1,61 @@
+package com.fexed.caputdraconis
+
+class SpellListUtility {
+    companion object {
+        fun Search(query: String, spellList: List<Spell>): List<Spell> {
+            val resultList = mutableListOf<Spell>()
+            val lookFor = query.lowercase()
+
+            for (spell in spellList) {
+                if (spell.nome.lowercase().contains(lookFor) ||
+                    spell.fonte.lowercase().contains(lookFor) ||
+                    spell.categoria.lowercase().contains(lookFor) ||
+                    spell.difinc.lowercase().contains(lookFor) ||
+                    spell.descrizione.lowercase().contains(lookFor)) {
+                    resultList.add(spell)
+                }
+            }
+
+            return resultList
+        }
+
+        fun SearchForName(name: String, spellList: List<Spell>): List<Spell> {
+            val resultList = mutableListOf<Spell>()
+            val lookFor = name.lowercase()
+
+            for (spell in spellList) {
+                if (spell.nome.lowercase().contains(lookFor)) {
+                    resultList.add(spell)
+                }
+            }
+
+            return resultList
+        }
+
+        fun SearchForDefensiveSpell(defensiveSpellName: String, spellList: List<Spell>): List<Spell> {
+            val resultList = mutableListOf<Spell>()
+            val lookFor = defensiveSpellName.lowercase()
+
+            for (spell in spellList) {
+                if (spell.difinc.lowercase().contains(lookFor)) {
+                    resultList.add(spell)
+                }
+            }
+
+            return resultList
+        }
+
+        fun SearchForSource(source: String, spellList: List<Spell>): List<Spell> {
+            val resultList = mutableListOf<Spell>()
+            val lookFor = source.lowercase()
+
+            for (spell in spellList) {
+                if (spell.fonte.lowercase().contains(lookFor)) {
+                    resultList.add(spell)
+                }
+            }
+
+            return resultList
+        }
+    }
+}
