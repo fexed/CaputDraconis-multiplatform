d14b6d5990518a8000e77189bb8a38e2182c4e63
Federico Matteoni <fedex15@live.it>
2022-10-24 21:24:50 +0200

    Common - update Platform

--- expelliarmus/src/commonMain/kotlin/com/fexed/caputdraconis/Platform.kt
+++ expelliarmus/src/commonMain/kotlin/com/fexed/caputdraconis/Platform.kt
@@ -2,7 +2,13 @@ package com.fexed.caputdraconis
 
 interface Platform {
     val name: String
-    val version: String
+    val appVersion: String
+    val commonVersion: String
+        get() = "v0.1"
+
+    fun getVersion(): String {
+        return "$name $appVersion - Expelliarmus $commonVersion"
+    }
 }
 
 expect fun getPlatform(): Platform
