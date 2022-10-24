e942ce2758eebb17233f6768fccde76e2196d510
Federico Matteoni <fedex15@live.it>
2022-10-24 21:25:00 +0200

    Android - update Platform

--- expelliarmus/src/androidMain/kotlin/com/fexed/caputdraconis/Platform.kt
+++ expelliarmus/src/androidMain/kotlin/com/fexed/caputdraconis/Platform.kt
@@ -1,8 +1,8 @@
 package com.fexed.caputdraconis
 
 class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-    override val version: String = "Expelliarmus v0.1\nAndroid v0.1"
+    override val name: String = "Android"
+    override val appVersion: String = "v0.1"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
