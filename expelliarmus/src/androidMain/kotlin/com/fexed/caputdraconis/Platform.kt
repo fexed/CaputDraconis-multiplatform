50ef038d9bf9485681c89c065319e7101863bfb7
Federico Matteoni <fedex15@live.it>
2022-11-05 19:16:51 +0100

    Android - Preferences

--- expelliarmus/src/androidMain/kotlin/com/fexed/caputdraconis/Platform.kt
+++ expelliarmus/src/androidMain/kotlin/com/fexed/caputdraconis/Platform.kt
@@ -1,8 +1,52 @@
 package com.fexed.caputdraconis
 
+import android.app.Activity
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+
 class AndroidPlatform : Platform {
     override val name: String = "Android"
     override val appVersion: String = "v0.1"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+
+actual typealias Preferences = Activity
+actual fun Preferences.getString(key: String, default: String) : String? {
+    val prefs: SharedPreferences = this.getSharedPreferences("", MODE_PRIVATE)
+    return prefs.getString(key, default)
+}
+
+
+actual fun Preferences.setString(key: String, value: String) {
+    val prefs: SharedPreferences = this.getSharedPreferences("", MODE_PRIVATE)
+    val editor = prefs.edit()
+    editor.putString(key, value)
+    editor.apply()
+}
+
+actual fun Preferences.getInt(key: String, default: Int) : Int? {
+    val prefs: SharedPreferences = this.getSharedPreferences("", MODE_PRIVATE)
+    return prefs.getInt(key, default)
+}
+
+
+actual fun Preferences.setInt(key: String, value: Int) {
+    val prefs: SharedPreferences = this.getSharedPreferences("", MODE_PRIVATE)
+    val editor = prefs.edit()
+    editor.putInt(key,value)
+    editor.apply()
+}
+
+actual fun Preferences.getBoolean(key: String, default: Boolean) : Boolean? {
+    val prefs: SharedPreferences = this.getSharedPreferences("", MODE_PRIVATE)
+    return prefs.getBoolean(key, default)
+}
+
+
+actual fun Preferences.setBoolean(key: String, value: Boolean) {
+    val prefs: SharedPreferences = this.getSharedPreferences("", MODE_PRIVATE)
+    val editor = prefs.edit()
+    editor.putBoolean(key, value)
+    editor.apply()
+}
