76a807f1f7f712008cff0cdf73ad342fbd7077aa
Federico Matteoni <fedex15@live.it>
2022-11-05 15:37:31 +0100

    Common - Testing random spell generation

---
+++ expelliarmus/src/commonTest/kotlin/com/fexed/caputdraconis/SpellListUtilityTest.kt
@@ -0,0 +1,60 @@
+package com.fexed.caputdraconis
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SpellListUtilityTest {
+    @Test
+    fun testRandomGenerationSimple() {
+        val spellList = mutableListOf<Spell>()
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Spell", "", "", "", ""))
+
+        val randomSpell = SpellListUtility.GetRandomSpell(spellList)
+
+        assertEquals("Spell", randomSpell.nome)
+    }
+
+    @Test
+    fun testRandomGenerationMedium() {
+        val spellList = mutableListOf<Spell>()
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("Spell", "", "", "", ""))
+
+        val randomSpell = SpellListUtility.GetRandomSpell(spellList)
+
+        assertEquals("Spell", randomSpell.nome)
+    }
+
+    @Test
+    fun testRandomGenerationMediumHard() {
+        val spellList = mutableListOf<Spell>()
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Spell", "", "", "", ""))
+
+        val randomSpell = SpellListUtility.GetRandomSpell(spellList)
+
+        assertEquals("Spell", randomSpell.nome)
+    }
+}
