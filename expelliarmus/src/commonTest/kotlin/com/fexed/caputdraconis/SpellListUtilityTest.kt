ad1c8c00eab8587ea97fb3f031492363c9571c36
Federico Matteoni <fedex15@live.it>
2022-11-05 15:39:47 +0100

    Common - Harder test
    A-OK!

--- expelliarmus/src/commonTest/kotlin/com/fexed/caputdraconis/SpellListUtilityTest.kt
+++ expelliarmus/src/commonTest/kotlin/com/fexed/caputdraconis/SpellListUtilityTest.kt
@@ -57,4 +57,82 @@ class SpellListUtilityTest {
 
         assertEquals("Spell", randomSpell.nome)
     }
+
+    @Test
+    fun testRandomGenerationHard() {
+        val spellList = mutableListOf<Spell>()
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("proTeGO", "", "", "", ""))
+        spellList.add(Spell("sCuTuM", "", "", "", ""))
+        spellList.add(Spell("FINITE", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Protego", "", "", "", ""))
+        spellList.add(Spell("Scutum", "", "", "", ""))
+        spellList.add(Spell("Finite", "", "", "", ""))
+        spellList.add(Spell("Spell", "", "", "", ""))
+
+        spellList.shuffle()
+
+        var randomSpell = SpellListUtility.GetRandomSpell(spellList)
+
+        assertEquals("Spell", randomSpell.nome)
+
+        spellList.shuffle()
+
+        randomSpell = SpellListUtility.GetRandomSpell(spellList)
+
+        assertEquals("Spell", randomSpell.nome)
+
+        spellList.shuffle()
+
+        randomSpell = SpellListUtility.GetRandomSpell(spellList)
+
+        assertEquals("Spell", randomSpell.nome)
+    }
 }
