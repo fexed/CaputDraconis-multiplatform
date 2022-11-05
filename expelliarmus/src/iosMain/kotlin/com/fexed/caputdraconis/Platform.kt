99a60f3fc79f1580689aef5a6ca4063546fcbff0
Federico Matteoni <fedex15@live.it>
2022-11-05 19:17:00 +0100

    iOS - Preferences

--- expelliarmus/src/iosMain/kotlin/com/fexed/caputdraconis/Platform.kt
+++ expelliarmus/src/iosMain/kotlin/com/fexed/caputdraconis/Platform.kt
@@ -1,8 +1,37 @@
 package com.fexed.caputdraconis
 
+import platform.Foundation.NSUserDefaults
+import platform.darwin.NSObject
+
 class IOSPlatform: Platform {
     override val name: String = "iOS"
     override val appVersion: String = "v0.1"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+actual typealias Preferences = NSObject
+
+actual fun Preferences.getInt(key: String, default: Int) : Int? {
+    return NSUserDefaults.standardUserDefaults.integerForKey(key).toInt()
+}
+
+actual fun Preferences.setInt(key: String, value: Int){
+    NSUserDefaults.standardUserDefaults.setInteger(value.toLong(), key)
+}
+
+actual fun Preferences.getString(key: String, default: String) : String? {
+    return NSUserDefaults.standardUserDefaults.stringForKey(key)
+}
+
+actual fun Preferences.setString(key: String, value: String){
+    NSUserDefaults.standardUserDefaults.setString(value, key)
+}
+
+actual fun Preferences.getBoolean(key: String, default: Boolean) : Boolean? {
+    return NSUserDefaults.standardUserDefaults.boolForKey(key)
+}
+
+actual fun Preferences.setBoolean(key: String, value: Boolean){
+    NSUserDefaults.standardUserDefaults.setBool(value, key)
+}
