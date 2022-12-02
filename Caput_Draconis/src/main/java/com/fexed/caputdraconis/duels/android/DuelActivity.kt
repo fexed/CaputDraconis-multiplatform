825a1c3bfc9f5a7b9adc7d659e2d65aa4e370c85
Federico Matteoni <fedex15@live.it>
2022-12-02 20:03:43 +0100

    Android - Timer mode button hidden from UI

--- Caput_Draconis/src/main/java/com/fexed/caputdraconis/duels/android/DuelActivity.kt
+++ Caput_Draconis/src/main/java/com/fexed/caputdraconis/duels/android/DuelActivity.kt
@@ -143,12 +143,12 @@ class DuelActivity : ComponentActivity() {
                     buttontxt = context.getString(R.string.play),
                     button = { context.startActivity(Intent(context, ArcadeModeActivity::class.java)) }
                 )
-                ModeCard(
-                    title = context.getString(R.string.timermode),
-                    desc = context.getString(R.string.timermodedesc),
-                    buttontxt = context.getString(R.string.play),
-                    button = { context.startActivity(Intent(context, TimerModeActivity::class.java)) }
-                )
+//                ModeCard(
+//                    title = context.getString(R.string.timermode),
+//                    desc = context.getString(R.string.timermodedesc),
+//                    buttontxt = context.getString(R.string.play),
+//                    button = { context.startActivity(Intent(context, TimerModeActivity::class.java)) }
+//                )
             }
         }
     }
