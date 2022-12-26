53dfb27b5db1ef65fcf5e817356908af20ae0a0a
Federico Matteoni <fedex15@live.it>
2022-12-26 10:46:32 +0100

    Common - Created Keys.kt

---
+++ expelliarmus/src/commonMain/kotlin/com/fexed/caputdraconis/analytics/Keys.kt
@@ -0,0 +1,12 @@
+package com.fexed.caputdraconis.analytics
+
+class Keys {
+    companion object {
+        const val KEY_APPOPEN = "KEY_APPOPEN"
+        const val KEY_GAMEOPEN = "KEY_GAMEOPEN"
+        const val KEY_ARCADE = "KEY_ARCADE"
+        const val KEY_TIMER = "KEY_TIMER"
+        const val KEY_SURVIVAL = "KEY_SURVIVAL"
+        const val KEY_GAMECLOSE = "KEY_GAMECLOSE"
+    }
+}
