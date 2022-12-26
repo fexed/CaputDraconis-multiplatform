cef0757fb57d579b77d575a4c93fefb79343dab3
Federico Matteoni <fedex15@live.it>
2022-12-26 10:46:24 +0100

    iOS - Added Firebase

--- Caput_Draconis/Caput_Draconis/iOSApp.swift
+++ Caput_Draconis/Caput_Draconis/iOSApp.swift
@@ -1,9 +1,20 @@
 import SwiftUI
 import AppTrackingTransparency
 import GoogleMobileAds
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     init() {
         if ATTrackingManager.trackingAuthorizationStatus == .notDetermined {
             //tracking not granted
